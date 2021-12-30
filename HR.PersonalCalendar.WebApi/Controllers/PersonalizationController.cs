1901ff85a632271ccc676b43dad7e28b53a4903d
jimatas <atask@hr.nl>
2021-12-30 13:41:42 +0100

    Personalization controller.

---
+++ HR.PersonalCalendar.WebApi/Controllers/PersonalizationController.cs
@@ -0,0 +1,34 @@
+﻿using Developist.Core.Cqrs;
+
+using HR.PersonalCalendar.Queries;
+using HR.PersonalCalendar.WebApi.Models;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Configuration;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR.PersonalCalendar.WebApi.Controllers
+{
+    public class PersonalizationController : ApiControllerBase
+    {
+        public PersonalizationController(
+            IWebHostEnvironment environment,
+            IConfiguration configuration,
+            IDispatcher dispatcher) : base(environment, configuration, dispatcher)
+        {
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<PersonalizationModel>> GetAsync([FromQuery(Name = "user"), BindRequired] string userName, CancellationToken cancellationToken = default)
+        {
+            var timetables = await QueryDispatcher.DispatchAsync(new GetPersonalTimetables { UserName = userName }, cancellationToken);
+            return timetables.Select(table => new PersonalizationModel(table));
+        }
+    }
+}
---
+++ HR.PersonalCalendar.WebApi/Models/PersonalizationModel.cs
@@ -0,0 +1,41 @@
+﻿using Developist.Core.Utilities;
+
+using HR.PersonalCalendar.Model;
+using HR.WebUntisConnector.Model;
+
+using System.Text.Json.Serialization;
+
+namespace HR.PersonalCalendar.WebApi.Models
+{
+    public class PersonalizationModel
+    {
+        public PersonalizationModel() { }
+
+        /// <summary>
+        /// Mapping constructor. 
+        /// Maps the properties of the specified <see cref="PersonalTimetable"/> entity to the corresponding properties in this <see cref="PersonalizationModel"/> object.
+        /// </summary>
+        /// <param name="table"></param>
+        public PersonalizationModel(PersonalTimetable table)
+        {
+            Ensure.Argument.NotNull(() => table);
+
+            UserName = table.UserName;
+            InstituteName = table.InstituteName;
+            ElementType = table.ElementType;
+            ElementId = table.ElementId;
+            ElementName = table.ElementName;
+            SchoolYearId = table.SchoolYearId;
+            IsVisible = table.IsVisible;
+        }
+
+        public string UserName { get; set; }
+        public string InstituteName { get; set; }
+        public ElementType ElementType { get; set; }
+        public int? ElementId { get; set; }
+        public string ElementName { get; set; }
+        public int? SchoolYearId { get; set; }
+        [JsonPropertyName("visible")]
+        public bool IsVisible { get; set; }
+    }
+}
