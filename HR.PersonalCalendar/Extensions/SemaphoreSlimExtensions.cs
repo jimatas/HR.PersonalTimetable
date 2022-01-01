a77963e882ae4a81483b1c4764a71c10242043a5
jimatas <atask@hr.nl>
2022-01-01 16:37:47 +0100

    Wait and release the semaphore in a using statement.

---
+++ HR.PersonalCalendar/Extensions/SemaphoreSlimExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Developist.Core.Utilities;
+
+using System;
+using System.Threading;
+
+namespace HR.PersonalCalendar.Extensions
+{
+    /// <summary>
+    /// Syntactic sugar for <see cref="SemaphoreSlim"/>.
+    /// </summary>
+    public static class SemaphoreSlimExtensions
+    {
+        /// <summary>
+        /// Allows the semaphore to be used in a using statement.
+        /// </summary>
+        /// <param name="semaphore"></param>
+        /// <returns></returns>
+        public static IDisposable WaitAndRelease(this SemaphoreSlim semaphore)
+        {
+            semaphore.Wait();
+            return new SemaphoreSlimReleaser(semaphore);
+        }
+
+        private class SemaphoreSlimReleaser : DisposableBase
+        {
+            private readonly SemaphoreSlim semaphore;
+            public SemaphoreSlimReleaser(SemaphoreSlim semaphore) => this.semaphore = semaphore;
+            protected override void ReleaseManagedResources()
+            {
+                semaphore.Release();
+                base.ReleaseManagedResources();
+            }
+        }
+    }
+}
