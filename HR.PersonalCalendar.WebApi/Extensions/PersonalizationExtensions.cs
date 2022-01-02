e7bc0a353baf751f8f267168ff5f9f87698b2818
jimatas <atask@hr.nl>
2022-01-02 11:17:21 +0100

    Expression body.

--- HR.PersonalCalendar.WebApi/Extensions/PersonalizationExtensions.cs
+++ HR.PersonalCalendar.WebApi/Extensions/PersonalizationExtensions.cs
@@ -8,17 +8,14 @@ namespace HR.PersonalCalendar.WebApi.Extensions
 {
     public static class PersonalizationExtensions
     {
-        public static Element ToElement(this Personalization personalization)
+        public static Element ToElement(this Personalization personalization) => personalization.ElementType switch
         {
-            return personalization.ElementType switch
-            {
-                ElementType.Klasse => new Klasse { Id = personalization.ElementId, Name = personalization.ElementName },
-                ElementType.Teacher => new Teacher { Id = personalization.ElementId, Name = personalization.ElementName },
-                ElementType.Subject => new Subject { Id = personalization.ElementId, Name = personalization.ElementName },
-                ElementType.Room => new Room { Id = personalization.ElementId, Name = personalization.ElementName },
-                ElementType.Student => new Student { Id = personalization.ElementId, Name = personalization.ElementName },
-                _ => throw new InvalidEnumArgumentException($"{nameof(personalization)}.{nameof(personalization.ElementType)}", Convert.ToInt32(personalization.ElementType), typeof(ElementType))
-            };
-        }
+            ElementType.Klasse => new Klasse { Id = personalization.ElementId, Name = personalization.ElementName },
+            ElementType.Teacher => new Teacher { Id = personalization.ElementId, Name = personalization.ElementName },
+            ElementType.Subject => new Subject { Id = personalization.ElementId, Name = personalization.ElementName },
+            ElementType.Room => new Room { Id = personalization.ElementId, Name = personalization.ElementName },
+            ElementType.Student => new Student { Id = personalization.ElementId, Name = personalization.ElementName },
+            _ => throw new InvalidEnumArgumentException($"{nameof(personalization)}.{nameof(personalization.ElementType)}", Convert.ToInt32(personalization.ElementType), typeof(ElementType))
+        };
     }
 }
