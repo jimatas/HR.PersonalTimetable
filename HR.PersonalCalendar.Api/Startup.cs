cc9e28d069e82824de39af493c62c18bfabee155
jimatas <atask@hr.nl>
2022-01-04 17:10:41 +0100

    Allow Swagger outside of development.

--- HR.PersonalCalendar.Api/Startup.cs
+++ HR.PersonalCalendar.Api/Startup.cs
@@ -86,9 +86,11 @@ namespace HR.PersonalCalendar.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HR.PersonalCalendar.Api v1"));
             }
+
+            // Use Swagger outside of development as well.
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("../swagger/v1/swagger.json", "HR.PersonalCalendar.Api v1"));
 
             app.UseHttpsRedirection();
 
