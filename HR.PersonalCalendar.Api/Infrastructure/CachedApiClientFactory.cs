4a8db10d3a82ebf953a1d74f65daf57d2ebde0f3
jimatas <atask@hr.nl>
2022-01-04 13:40:32 +0100

    Checking for valid school or institute name.

--- HR.PersonalCalendar.Api/Infrastructure/CachedApiClientFactory.cs
+++ HR.PersonalCalendar.Api/Infrastructure/CachedApiClientFactory.cs
@@ -1,9 +1,13 @@
 ﻿using Developist.Core.Utilities;
 
 using HR.PersonalCalendar.Api.Extensions;
+using HR.PersonalCalendar.Api.Models;
 using HR.WebUntisConnector;
+using HR.WebUntisConnector.Configuration;
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace HR.PersonalCalendar.Api.Infrastructure
@@ -14,11 +18,15 @@ namespace HR.PersonalCalendar.Api.Infrastructure
     public class CachedApiClientFactory : ICachedApiClientFactory
     {
         private readonly IApiClientFactory apiClientFactory;
+        private readonly WebUntisConfigurationSection configuration;
         private readonly IDictionary<string, CachedApiClient> cachedApiClients = new Dictionary<string, CachedApiClient>();
         private readonly SemaphoreSlim mutex = new(1, 1);
 
-        public CachedApiClientFactory(IApiClientFactory apiClientFactory)
-            => this.apiClientFactory = Ensure.Argument.NotNull(() => apiClientFactory);
+        public CachedApiClientFactory(IApiClientFactory apiClientFactory, WebUntisConfigurationSection configuration)
+        {
+            this.apiClientFactory = Ensure.Argument.NotNull(() => apiClientFactory);
+            this.configuration = Ensure.Argument.NotNull(() => configuration);
+        }
 
         /// <inheritdoc/>
         public IApiClient CreateApiClient(string schoolOrInstituteName, out string userName, out string password)
@@ -33,12 +41,25 @@ namespace HR.PersonalCalendar.Api.Infrastructure
                 }
                 else
                 {
+                    EnsureSchoolOrInstituteExists(schoolOrInstituteName);
                     apiClient = apiClientFactory.CreateApiClient(schoolOrInstituteName, out userName, out password);
                     cachedApiClients.Add(schoolOrInstituteName, new(apiClient, userName, password));
                 }
             }
 
             return apiClient;
+        }
+
+        /// <summary>
+        /// Throws <see cref="NotFoundException"/> if <paramref name="schoolOrInstituteName"/> does not refer to a valid school or institute configuration element.
+        /// </summary>
+        /// <param name="schoolOrInstituteName"></param>
+        private void EnsureSchoolOrInstituteExists(string schoolOrInstituteName)
+        {
+            if (!configuration.Schools.Any(school => school.Name.Equals(schoolOrInstituteName, StringComparison.InvariantCultureIgnoreCase) || school.Institutes.Any(institute => institute.Name.Equals(schoolOrInstituteName, StringComparison.InvariantCultureIgnoreCase))))
+            {
+                throw new NotFoundException($"No school or institute with the name {schoolOrInstituteName} found.");
+            }
         }
 
         private record CachedApiClient(IApiClient ApiClient, string UserName, string Password);
