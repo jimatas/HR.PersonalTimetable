5b22621a46998f6ba8c3fef0996306e972927030
jimatas <atask@hr.nl>
2022-01-12 17:32:56 +0100

    Endpoint for retrieving last import time.

--- HR.PersonalTimetable.Api/Controllers/InstitutesController.cs
+++ HR.PersonalTimetable.Api/Controllers/InstitutesController.cs
@@ -1,14 +1,19 @@
-﻿using Developist.Core.Utilities;
+﻿using Developist.Core.Cqrs.Queries;
+using Developist.Core.Utilities;
 
 using HR.PersonalTimetable.Api.Models;
+using HR.PersonalTimetable.Api.Queries;
 using HR.WebUntisConnector.Configuration;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mime;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HR.PersonalTimetable.Api.Controllers
 {
@@ -17,11 +22,13 @@ namespace HR.PersonalTimetable.Api.Controllers
     [Produces(MediaTypeNames.Application.Json)]
     public class InstitutesController : ControllerBase
     {
+        private readonly IQueryDispatcher queryDispatcher;
         private readonly WebUntisConfigurationSection configuration;
 
-        public InstitutesController(WebUntisConfigurationSection configuration)
+        public InstitutesController(IQueryDispatcher queryDispatcher, WebUntisConfigurationSection configuration)
         {
             this.configuration = Ensure.Argument.NotNull(() => configuration);
+            this.queryDispatcher = Ensure.Argument.NotNull(() => queryDispatcher);
         }
 
         [HttpGet]
@@ -30,6 +37,13 @@ namespace HR.PersonalTimetable.Api.Controllers
         {
             var institutes = configuration.Schools.SelectMany(school => school.Institutes).Where(institute => institute.IsVisible).Select(Institute.FromInstituteElement);
             return institutes;
+        }
+
+        [HttpGet("{institute}/last-imported")]
+        [ProducesResponseType(StatusCodes.Status200OK), ProducesResponseType(StatusCodes.Status400BadRequest), ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<DateTime> GetLastImportAsync([FromRoute] GetLastImportTime query, CancellationToken cancellationToken = default)
+        {
+            return await queryDispatcher.DispatchAsync(query, cancellationToken);
         }
     }
 }
