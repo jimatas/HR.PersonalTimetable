ccfaf409ae7ddb8e6ac010116cab8b46acfca767
jimatas <atask@hr.nl>
2022-01-23 01:11:03 +0100

    Updated swagger doc to v2. Minor edits.

--- HR.PersonalTimetable.Api/Models/Schedule.cs
+++ HR.PersonalTimetable.Api/Models/Schedule.cs
@@ -11,8 +11,12 @@ namespace HR.PersonalTimetable.Api.Models
     /// </summary>
     public class Schedule
     {
+        /// <example>2022-01-17T00:00:00.000Z</example>
         public DateTime StartDate { get; set; }
+        
+        /// <example>2022-01-21T00:00:00.000Z</example>
         public DateTime EndDate { get; set; }
+
         public Institute Institute { get; set; }
         public Element Element { get; set; }
         public IEnumerable<Holiday> Holidays { get; set; }
