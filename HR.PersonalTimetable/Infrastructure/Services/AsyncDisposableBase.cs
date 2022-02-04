e4e69a0027f552af9984289fefb68747c3166f2b
jimatas <atask@hr.nl>
2022-02-04 10:31:59 +0100

    Introduced caching wrapper for IApiClient, which defers signing out until disposal and handles the WebUntis invalid schoolname error.

---
+++ HR.PersonalTimetable/Infrastructure/Services/AsyncDisposableBase.cs
@@ -0,0 +1,40 @@
+﻿using Developist.Core.Utilities;
+
+using System;
+using System.Threading.Tasks;
+
+namespace HR.PersonalTimetable.Infrastructure.Services
+{
+    public class AsyncDisposableBase : DisposableBase, IAsyncDisposable
+    {
+        public async ValueTask DisposeAsync()
+        {
+            await DisposeAsyncCore().ConfigureAwait(false);
+            Dispose(disposing: false);
+
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Called to asynchronously dispose of managed resources.
+        /// </summary>
+        /// <returns>An awaitable value task representing the asynchronous operation.</returns>
+        protected virtual async ValueTask DisposeAsyncCore()
+        {
+            if (IsDisposed)
+            {
+                return;
+            }
+
+            await ReleaseManagedResourcesAsync().ConfigureAwait(false);
+
+            IsDisposed = true;
+        }
+
+        /// <summary>
+        /// Override to asynchronously release any managed resources held by this instance.
+        /// </summary>
+        /// <returns>An awaitable value task representing the asynchronous operation.</returns>
+        protected virtual ValueTask ReleaseManagedResourcesAsync() => default;
+    }
+}
