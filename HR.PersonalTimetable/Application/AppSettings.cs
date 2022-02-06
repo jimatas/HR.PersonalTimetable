f4f0f126bca1083a3eca748aec323f2e8e7cb302
jimatas <atask@hr.nl>
2022-02-06 12:07:32 +0100

    Renamed some appsettings variables. Added a webuntis.production.config file.

--- HR.PersonalTimetable/Application/AppSettings.cs
+++ HR.PersonalTimetable/Application/AppSettings.cs
@@ -8,27 +8,27 @@ namespace HR.PersonalTimetable.Application
         /// The maximum date range for which timetables can be requested.
         /// Too large of a value will excessively stress the server. Default value is 30 days.
         /// </summary>
-        public int MaxDaysInTimetableDateRange { get; set; } = 30;
+        public int TimetableMaxDays { get; set; } = 30;
 
         /// <summary>
         /// The number of additional weeks prior to the current one to include in the calendar export.
         /// Default value is 1 week.
-        /// Note that the date range created by <see cref="NumberOfWeeksBeforeCurrentToExport"/> and <see cref="NumberOfWeeksAfterCurrentToExport"/> must not exceed <see cref="MaxDaysInTimetableDateRange"/>.
+        /// Note that the date range created by <see cref="CalendarWeeksInPast"/> and <see cref="CalendarWeeksInFuture"/> must not exceed <see cref="TimetableMaxDays"/>.
         /// </summary>
-        public int NumberOfWeeksBeforeCurrentToExport { get; set; } = 1;
+        public int CalendarWeeksInPast { get; set; } = 1;
 
         /// <summary>
         /// The number of additional weeks after the current one to include in the calendar export.
         /// Default value is 2 weeks.
-        /// Note that the date range created by <see cref="NumberOfWeeksBeforeCurrentToExport"/> and <see cref="NumberOfWeeksAfterCurrentToExport"/> must not exceed <see cref="MaxDaysInTimetableDateRange"/>.
+        /// Note that the date range created by <see cref="CalendarWeeksInPast"/> and <see cref="CalendarWeeksInFuture"/> must not exceed <see cref="TimetableMaxDays"/>.
         /// </summary>
-        public int NumberOfWeeksAfterCurrentToExport { get; set; } = 2;
+        public int CalendarWeeksInFuture { get; set; } = 2;
 
         /// <summary>
         /// The suggested refresh interval that an external calendar should poll for calendar updates.
         /// Too small of a value will excessively stress the server. Default value is 30 minutes.
         /// </summary>
-        public TimeSpan ExportRefreshInterval { get; set; } = TimeSpan.FromMinutes(30);
+        public TimeSpan CalendarRefreshInterval { get; set; } = TimeSpan.FromMinutes(30);
 
         /// <summary>
         /// Clock skew to account for between client and server.
