21fb9e87263abd74b212c67e119e51291f13ade3
jimatas <atask@hr.nl>
2022-03-04 11:42:19 +0100

    New holidays endpoint.

--- HR.PersonalTimetable/Api/Controllers/InstitutesController.cs
+++ HR.PersonalTimetable/Api/Controllers/InstitutesController.cs
@@ -3,6 +3,7 @@ using Developist.Core.Utilities;
 
 using HR.PersonalTimetable.Application.Models;
 using HR.PersonalTimetable.Application.Queries;
+using HR.WebUntisConnector.Model;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -49,6 +50,19 @@ namespace HR.PersonalTimetable.Api.Controllers
         [HttpGet("{institute}/last-imported")]
         [ProducesResponseType(StatusCodes.Status200OK), ProducesResponseType(StatusCodes.Status400BadRequest), ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<DateTime> GetLastImportedAsync([FromRoute] GetLastImportTime query, CancellationToken cancellationToken = default)
+        {
+            return await queryDispatcher.DispatchAsync(query, cancellationToken);
+        }
+
+        /// <summary>
+        /// Retrieve all the holidays defined for a particular RUAS institute.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpGet("{institute}/holidays")]
+        [ProducesResponseType(StatusCodes.Status200OK), ProducesResponseType(StatusCodes.Status400BadRequest), ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IEnumerable<Holiday>> GetHolidaysAsync([FromRoute, FromQuery] GetHolidays query, CancellationToken cancellationToken = default)
         {
             return await queryDispatcher.DispatchAsync(query, cancellationToken);
         }
